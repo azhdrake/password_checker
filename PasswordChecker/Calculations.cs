0e1529b81a4ee54fe3bf7794f2c68743cff6e06a
B33 <my@email.com>
2020-01-30 13:04:46 -0600

    Separate large chunks of code into separate methods

--- PasswordChecker/Calculations.cs
+++ PasswordChecker/Calculations.cs
@@ -8,5 +8,57 @@ namespace PasswordChecker
 {
   static public class Calculations
   {
+    static public Dictionary<String, bool> makeDictionary()
+    {
+      //Bools are to keep track of the ways a password can be secure.
+      Dictionary<String, bool> ChecksDict = new Dictionary<String, bool>();
+      ChecksDict.Add("specialChar", false);
+      ChecksDict.Add("lengthTen", false);
+      ChecksDict.Add("number", false);
+      ChecksDict.Add("capitalChar", false);
+      ChecksDict.Add("lowerChar", false);
+      ChecksDict.Add("space", false);
+      return ChecksDict;
+    }
+
+    static public void setBools(String userPassword, Dictionary<String, bool> checksDict)
+    {
+      if (userPassword.Any(char.IsDigit))
+      {
+        checksDict["number"] = true;
+      }
+      if (userPassword.Any(char.IsLower))
+      {
+        checksDict["lowerChar"] = true;
+      }
+      if (userPassword.Any(char.IsUpper))
+      {
+        checksDict["capitalChar"] = true;
+      }
+      if (userPassword.Any(char.IsSymbol) || userPassword.Any(char.IsPunctuation))
+      {
+        checksDict["specialChar"] = true;
+      }
+      if (userPassword.Length > 10)
+      {
+        checksDict["lengthTen"] = true;
+      }
+      if (userPassword.Any(char.IsWhiteSpace))
+      {
+        checksDict["space"] = true;
+      }
+    }
+    static public int countBools(Dictionary<String, bool> checksDict)
+    {
+      int checkCounter = 0;
+      foreach (bool check in checksDict.Values)
+      {
+        if (check)
+        {
+          checkCounter++;
+        }
+      }
+      return checkCounter;
+    }
   }
 }
--- PasswordChecker/Form1.cs
+++ PasswordChecker/Form1.cs
@@ -18,54 +18,18 @@ namespace PasswordChecker
     }
 
     private void button1_Click(object sender, EventArgs e)
-      { 
-      //Bools are to keep track of the ways a password can be secure.
-      Dictionary<String, bool> ChecksDict = new Dictionary<String, bool>();
-      ChecksDict.Add("specialChar", false);
-      ChecksDict.Add("lengthTen", false);
-      ChecksDict.Add("number", false);
-      ChecksDict.Add("capitalChar", false);
-      ChecksDict.Add("lowerChar", false);
-      ChecksDict.Add("space", false);
-
+    {
       String userPassword = txtPassword.Text;
 
-      //sets bool to true if that security measure is accounted for in the user's password
-      if (userPassword.Any(char.IsDigit))
-      {
-        ChecksDict["number"] = true;
-      }
-      if (userPassword.Any(char.IsLower))
-      {
-        ChecksDict["lowerChar"] = true;
-      }
-      if (userPassword.Any(char.IsUpper))
-      {
-        ChecksDict["capitalChar"] = true;
-      }
-      if (userPassword.Any(char.IsSymbol) || userPassword.Any(char.IsPunctuation))
-      {
-        ChecksDict["specialChar"] = true;
-      }
-      if (userPassword.Length > 10)
-      {
-        ChecksDict["lengthTen"] = true;
-      }
-      if (userPassword.Any(char.IsWhiteSpace))
-      {
-        ChecksDict["space"] = true;
-      }
+      //makes a dictionary of bools to keep track of the security measures that we thought were relivant.
+      //We didn't look anything up, we just went based on our knowledge of password security.
+      Dictionary<String, bool> checksDict = Calculations.makeDictionary();
 
-      int checkCounter = 0;
+      //sets bool to true if that security measure is accounted for in the user's password
+      Calculations.setBools(userPassword, checksDict);
 
       // checks how many security measures are accounted for
-      foreach(bool check in ChecksDict.Values)
-      {
-        if (check)
-        {
-          checkCounter ++;
-        }
-      }
+      int checkCounter = Calculations.countBools(checksDict);
 
       // looks at the number of security measures used and rates them.
       if (checkCounter <= 2)
@@ -88,7 +52,6 @@ namespace PasswordChecker
         lblStrength.Text = "Very Good!";
         BackColor = Color.Green;
       }
-
     }
   }
 }
