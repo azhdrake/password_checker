13d3b13218334b5f34f1718b512376ab70ced10b
B33 <my@email.com>
2020-01-30 12:35:24 -0600

    Add if statement using security bools to determine final result

--- PasswordChecker/Form1.Designer.cs
+++ PasswordChecker/Form1.Designer.cs
@@ -48,7 +48,6 @@
       // 
       this.txtPassword.Location = new System.Drawing.Point(265, 31);
       this.txtPassword.Name = "txtPassword";
-      this.txtPassword.PasswordChar = '*';
       this.txtPassword.Size = new System.Drawing.Size(202, 31);
       this.txtPassword.TabIndex = 1;
       // 
--- PasswordChecker/Form1.cs
+++ PasswordChecker/Form1.cs
@@ -21,53 +21,79 @@ namespace PasswordChecker
     {
       bool specialChar = false;
       bool lengthTen = false;
-      bool letters = false;
       bool number = false;
       bool capitalChar = false;
       bool lowerChar = false;
+      bool space = false;
 
       String userPassword = txtPassword.Text;
+      String testing = "";
 
       if (userPassword.Any(char.IsDigit))
       {
         number = true;
-      }
-      if (userPassword.Any(char.IsLetter))
-      {
-        letters = true;
+        testing += "Digit ";
       }
       if (userPassword.Any(char.IsLower))
       {
         lowerChar = true;
+        testing += "Lower ";
       }
       if (userPassword.Any(char.IsUpper))
       {
         capitalChar = true;
+        testing += "Capital ";
       }
-      if (!userPassword.Any(char.IsLetterOrDigit))
+      if (userPassword.Any(char.IsSymbol) || userPassword.Any(char.IsPunctuation))
       {
         specialChar = true;
+        testing += "Special ";
       }
       if (userPassword.Length > 10)
       {
         lengthTen = true;
+        testing += "Length ";
+      }
+      if (userPassword.Any(char.IsWhiteSpace))
+      {
+        space = true;
+        testing += "Space ";
       }
 
-        specialChar = true;
-      bool[] checksList = new bool[] { number, letters, lowerChar, capitalChar, specialChar, lengthTen };
+      bool[] checksList = new bool[] { number, lowerChar, capitalChar, specialChar, lengthTen, space};
 
-      int counter = 0;
+      int checkCounter = 0;
 
       foreach(bool check in checksList)
       {
         if (check)
         {
-          counter ++;
+          checkCounter ++;
         }
       }
 
-
+      if (checkCounter <= 2)
+      {
+        lblStrength.Text = "Bad!";
+        BackColor = Color.Red;
       }
+      else if (checkCounter >= 3 && checkCounter <= 4)
+      {
+        lblStrength.Text = "Okay!";
+        BackColor = Color.Yellow;
+      }
+      else if (checkCounter == 5)
+      {
+        lblStrength.Text = "Good!";
+        BackColor = Color.GreenYellow;
+      }
+      else if (checkCounter == 6)
+      {
+        lblStrength.Text = "Very Good!";
+        BackColor = Color.Green;
+      }
+      MessageBox.Show(testing);
+
     }
   }
 }
